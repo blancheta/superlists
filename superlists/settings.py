4e73f6905e929e065385d159427b40f1072be928
Alexandre Blanchet <alexandreblanchet44@gmail.com>
2016-05-03 20:22:11 +0100

    change path database 3

--- superlists/settings.py
+++ superlists/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': '../database/db.sqlite3',
     }
 }
 
