2f73ca01a6b98c47d80eba0fad30169ecb6bdfc1
Alexandre Blanchet <alexandreblanchet44@gmail.com>
2016-05-03 17:59:35 +0100

    settings database path modif

--- superlists/settings.py
+++ superlists/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g2x5^l6tv_0!$u07n7v5-k3pg)0&6%72*5y&^wa-96kfb+3*^@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+TEMPLATE_DEBUG = False
+
+ALLOWED_HOSTS = ['aldazar-superlists.ddns.net']
 
 
 # Application definition
@@ -78,7 +80,7 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
     }
 }
 
