9c4eccbdb4d0ce3c92cda9279e6ba21927128180
Alexandre Blanchet <alexandreblanchet44@gmail.com>
2016-05-13 16:33:05 +0100

    ExistingListItemForm class + test creation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -4,7 +4,7 @@ class ItemValidationTest(FunctionalTest):
 
 	def test_cannot_add_empty_list_items(self):
 
-		# Edith goes to the home page amd accidentally tries to submit
+		# Edith goes to the home page and accidentally tries to submit
 
 		# an empty list item. She hits Enter on the empty input box.
 		self.browser.get(self.server_url)
@@ -34,6 +34,20 @@ class ItemValidationTest(FunctionalTest):
 		self.get_item_input_box().send_keys('Make tea\n')
 		self.check_for_row_in_list_table('1: Buy milk')
 		self.check_for_row_in_list_table('2: Make tea')
+
+	def test_cannot_add_duplicate_items(self):
+
+		# Edith goes to the home page and starts a new list
+		self.browser.get(self.server_url)
+		self.get_item_input_box().send_keys('Buy wellies\n')
+		self.check_for_row_in_list_table('1: Buy wellies')
+
+		# She accidentally tries to enter a duplicate item
+		self.get_item_input_box().send_keys('Buy wellies\n')
+		self.check_for_row_in_list_table('1: Buy wellies')
+		# She sees a helpful error message
+		error = self.browser.find_element_by_css_selector('.has-error')
+		self.assertEqual(error.text, "You've already got this in your list")
 
 # if __name__ == '__main__':
 # 	unittest.main(warnings='ignore')
