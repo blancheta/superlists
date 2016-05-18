1b289de78ff76bf4b0302f12c1cc2387bd920291
Alexandre Blanchet <alexandreblanchet44@gmail.com>
2016-05-18 13:53:21 +0100

    js testing

--- todolists/static/list.js
+++ todolists/static/list.js
@@ -1,16 +1,5 @@
-$('input').on('keypress', function(){
-	$('.has-error').hide();
-});
-
-
-
-/*global $, test, equal */
-test("erros should be hidden on keypress", function(){
-	$('input').trigger('keypress');
-	equal($('.has-error').is(':visible'), false);
-});
-
-test("errors not be hidden unless there is a keypress",
-	function(){
-		equal($('.has-error').is(':visible'), true);
+Jquery(document).ready(function($){
+	$('input').on('keypress', function(){
+		$('.has-error').hide();
+	});
 });
