3d718d3c7e962c62d57799ba7a98402f5db20d45
Alexandre Blanchet <alexandreblanchet44@gmail.com>
2016-05-27 16:20:36 +0100

    fix DOMAIN

--- accounts/authentication.py
+++ accounts/authentication.py
@@ -1,6 +1,7 @@
 import requests
 from django.contrib.auth import get_user_model
 from superlists.settings import DOMAIN
+import logging
 User = get_user_model()
 
 
@@ -10,12 +11,14 @@ PERSONA_VERIFY_URL = 'https://verifier.login.persona.org/verify'
 class PersonaAuthenticationBackend(object):
 
 	def authenticate(self, assertion):
+		logging.warning('entering authenticate function')
 		# Send the assertion to Mozilla's verifier service.
-		print("Log in ....")
 		response = requests.post(
 			PERSONA_VERIFY_URL,
 			data={'assertion': assertion, 'audience': DOMAIN}
 		)
+		logging.warning('got response from persona')
+		logging.warning(response.content.decode())
 
 		if response.ok and response.json()['status'] == 'okay':
 			email = response.json()['email']
