0ae6270e4cc44abddb68393879fac7f590255897
Alexandre Blanchet <alexandreblanchet44@gmail.com>
2016-05-30 20:57:39 +0100

    update Command create_session

--- manage.py
+++ manage.py
@@ -1,16 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
-import django
-from django.conf import settings
-from superlists import settings as mysettings
 
 if __name__ == "__main__":
 
 	os.environ['DJANGO_SETTINGS_MODULE'] = "superlists.settings"
 	# sys.path.append('/home/aldazar/sites/staging.aldazar-superlists.ddns.net/source')
-	sys.path.append('/source')
 	from django.core.management import execute_from_command_line
 	execute_from_command_line(sys.argv)
-
-
